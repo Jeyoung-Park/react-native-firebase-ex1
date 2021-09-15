85de1b549614e9e930d30c1e1d713fdea7db1b42
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-09-15 21:42:41 +0900

    feat:InputView UI

--- src/InputVIew.js
+++ src/InputVIew.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, SafeAreaView, TextInput, Button } from 'react-native';
 
 const InputView = ({
     params,
-}) => (
-    <View>
-        <Text>InputView</Text>
-    </View>
-);
+}) => {
+
+    const [text, setText]=useState('');
+
+    return(
+        <SafeAreaView style={{flex:1,}}>
+            <Text>메시지 입력하기</Text>
+            <TextInput 
+                placeholder='메시지를 입력해주세요.'
+                value={text}
+                onChangeText={(text)=>setText(text)}
+            />
+            <Button 
+                title='제출하기'
+                onPress={()=>{
+                    console.log('text in 제출하기, ', text);
+                    setText('');
+                }}
+            />
+        </SafeAreaView>
+    )
+}
 
 export default InputView;
