34a18e57ac8a8c00e6e77f8649c7c8ee705960f9
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-09-15 21:50:24 +0900

    feat: 추가 메서드 추가

--- src/InputVIew.js
+++ src/InputVIew.js
@@ -1,11 +1,26 @@
 import React, { useState } from 'react';
 import { Text, View, SafeAreaView, TextInput, Button } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 const InputView = ({
     params,
 }) => {
 
     const [text, setText]=useState('');
+
+    const postValue=()=>{
+        firestore()
+        .collection('Collection1')
+        // .doc('y392j6x7RiVicabwSxhJ')
+        .add({
+            loginId:'id',
+            username:'name',
+            message:text,
+        })
+        .then(()=>{
+            console.warn('추가가 성공하였습니다.');
+        })
+    }
 
     return(
         <SafeAreaView style={{flex:1,}}>
@@ -19,6 +34,7 @@ const InputView = ({
                 title='제출하기'
                 onPress={()=>{
                     console.log('text in 제출하기, ', text);
+                    postValue();
                     setText('');
                 }}
             />
