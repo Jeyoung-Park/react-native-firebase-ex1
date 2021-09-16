0cd6e1490fe310446355f7d81d8da75419c7bbb2
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-09-16 23:04:57 +0900

    feat: text recognition 추가

--- src/TextRecognitionVIew.js
+++ src/TextRecognitionVIew.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import {Button, SafeAreaView, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Button, SafeAreaView, Text, View, Image} from 'react-native';
 import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import MlkitOcr from 'react-native-mlkit-ocr';
 
 const TextRecognitionView = ({params}) => {
-  const onImageSelect = (media) => {
+  const [image, setImage] = useState();
+  const [result, setResult] = useState({});
+
+  console.log('image, ', image);
+  console.log('result, ', result);
+
+  const onImageSelect = async media => {
     // console.log('onImageSelect');
-    if(!media.didCancel){
-        
-    }
+    console.log('onImageSelect, ', media);
+    // if (!media.didCancel) {
+    //   setImage(media.assets[0].uri);
+    // }
+    setImage(media.assets[0].uri);
+    // const processingResult = await ml().cloudDocumentTextRecognizerProcessImage(
+    //   media.assets[0].uri
+    // );
+    // console.log(processingResult);
+    // setResult(processingResult);
+    // const processed = await vision().cloudDocumentTextRecognizerProcessImage(media.assets[0].uri);
+    // console.log('Found text in document: ', processed);
+    // setResult(processed.text)
+    const resultFromFile = await MlkitOcr.detectFromFile(media.assets[0].uri);
+    console.log('resultFromFile, ', resultFromFile);
   };
 
   const onTakePhoto = () => {
@@ -23,6 +42,10 @@ const TextRecognitionView = ({params}) => {
       style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Button title="사진 찍기" onPress={onTakePhoto} />
       <Button title="앨범에서 가져오기" onPress={onSelectImagePress} />
+      <Text>Image</Text>
+      <Image style={{width: 200, height: 200}} source={{uri: image}} />
+      <Text>Result</Text>
+      <Text>{result.text}</Text>
     </SafeAreaView>
   );
 };
