a0aa4ce0c6893d6acef2be04abd508f326786c47
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-09-16 21:51:56 +0900

    feat: TextRecognitionView 카메라 버튼 UI

--- src/TextRecognitionVIew.js
+++ src/TextRecognitionVIew.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {Button, SafeAreaView, Text, View} from 'react-native';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
-const TextRecognitionView = ({
-    params,
-}) => (
-    <View>
-        <Text>TextRecognitionView</Text>
-    </View>
-);
+const TextRecognitionView = ({params}) => {
+  const onImageSelect = (media) => {
+    // console.log('onImageSelect');
+    if(!media.didCancel){
+        
+    }
+  };
+
+  const onTakePhoto = () => {
+    launchCamera({mediaType: 'photo'}, onImageSelect);
+  };
+
+  const onSelectImagePress = () => {
+    launchImageLibrary({mediaType: 'photo'}, onImageSelect);
+  };
+
+  return (
+    <SafeAreaView
+      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Button title="사진 찍기" onPress={onTakePhoto} />
+      <Button title="앨범에서 가져오기" onPress={onSelectImagePress} />
+    </SafeAreaView>
+  );
+};
 
 export default TextRecognitionView;
