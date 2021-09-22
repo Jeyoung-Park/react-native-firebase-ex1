e48ae5e17b6a8856b529dbfe32dd0561be86b2e7
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-09-22 18:36:54 +0900

    feat: push 알림 구현

--- App.js
+++ App.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {Alert, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Main from './src/Main';
 import InputView from './src/InputVIew';
 import ReadView from './src/ReadView';
 import TextRecognitionView from './src/TextRecognitionVIew';
+import messaging from '@react-native-firebase/messaging';
 
 const Stack = createNativeStackNavigator();
 
-const App = ({params}) => (
-  <NavigationContainer>
-    <Stack.Navigator initialRouteName="Main">
-      <Stack.Screen name="Main" component={Main} />
-      <Stack.Screen name="InputView" component={InputView} />
-      <Stack.Screen name="ReadView" component={ReadView} />
-      <Stack.Screen name="TextRecognitionView" component={TextRecognitionView} />
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+const App = ({params}) => {
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('FCM message, ', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen name="Main" component={Main} />
+        <Stack.Screen name="InputView" component={InputView} />
+        <Stack.Screen name="ReadView" component={ReadView} />
+        <Stack.Screen
+          name="TextRecognitionView"
+          component={TextRecognitionView}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;
