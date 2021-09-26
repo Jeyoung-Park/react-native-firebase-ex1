da7d9614c9fcf93c8705a6438086b31a42a7ef42
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-09-26 21:12:08 +0900

    feat: 이메일 로그인

--- src/GoogleSignInView.js
+++ src/GoogleSignInView.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import {Button} from 'react-native';
+import React, {useState} from 'react';
+import {Button, TextInput, Text, View, StyleSheet} from 'react-native';
 import auth from '@react-native-firebase/auth';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
+export default function GoogleSignInView({login, setLogin}) {
+  const [id, setId] = useState();
+  const [pw, setPw] = useState();
 
+  const onEmailButtonPress = async () => {
+    auth()
+      .createUserWithEmailAndPassword(id, pw)
+      .then(() => {
+        console.log('User account created & signed in!');
+      })
+      .catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('That email address is already in use!');
+        }
 
-export default function GoogleSignInView({
-  login,
-  setLogin,
-}) {
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+        }
+
+        console.error(error);
+      });
+  };
+
   async function onGoogleButtonPress() {
     console.log('onGoogleButtonPress Clicked');
 
@@ -23,14 +40,50 @@ export default function GoogleSignInView({
     // Sign-in the user with the credential
     return auth().signInWithCredential(googleCredential);
   }
+
   return (
-    <Button
-      title="Google Sign-In"
-      onPress={() =>
-        onGoogleButtonPress().then(() => {
-          setLogin(true);
-        })
-      }
-    />
+    <View>
+      <Text>ID</Text>
+      <TextInput
+        style={styles.textinput}
+        placeholder="ID"
+        onChangeText={text => setId(text)}
+      />
+      <Text>PW</Text>
+      <TextInput
+        style={styles.textinput}
+        placeholder="Password"
+        onChangeText={text => setPw(text)}
+        textContentType={'password'}
+      />
+      <Button
+        title="이메일로 로그인"
+        onPress={() => {
+          onEmailButtonPress().then(() => {
+            setLogin(true);
+          });
+        }}
+      />
+      <Button
+        title="Google Sign-In"
+        onPress={() =>
+          onGoogleButtonPress().then(() => {
+            setLogin(true);
+          })
+        }
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  textinput: {
+    width: 300,
+
+    borderColor: 'black',
+    borderWidth: 1,
+
+    marginTop: 16,
+    marginBottom: 24,
+  },
+});
